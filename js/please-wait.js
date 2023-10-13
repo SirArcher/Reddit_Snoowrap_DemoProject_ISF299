e7f25693b02489e8f56b07ed2ec59c9a74e51ec3
SirArcher <87906940+SirArcher@users.noreply.github.com>
2023-10-13 13:16:01 +0300

    redirect route update

--- js/please-wait.js
+++ js/please-wait.js
@@ -2,7 +2,7 @@ fetch('/please-wait/process-data', {credentials: 'include'})
     .then(response => response.json())
     .then(data => {
         if (data.success === true) {
-            window.location.href = '/userpanel'; 
+            window.location.href = '/user-navigation'; 
         }
         else if (data.success === false) {
             window.location.href = '/error'; 
